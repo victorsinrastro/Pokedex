187660c8d55821d6bea3ba30a35ceecefebb8c8f
victoribarra <victor.ibarra.garcia1@gmail.com>
2024-02-12 21:46:21 -0800

    Covert to MVVM.

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -1,54 +1,48 @@
 package com.example.pokedex
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pokedex.databinding.ActivityMainBinding
-import com.example.pokedex.network.PokemonLoader
-import com.example.pokedex.network.models.Pokemon
-import com.example.pokedex.network.models.PokemonListResponse
 import com.example.pokedex.pokemon.PokemonAdapter
-import com.example.pokedex.utils.Constants
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.example.pokedex.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val loader = PokemonLoader()
-    private val call = loader.getPokemonList()
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.progressBarVisibility = true
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        setupObservers()
+        setupRecyclerView()
+    }
 
-        call.enqueue(object : Callback<PokemonListResponse> {
-            override fun onResponse(
-                call: Call<PokemonListResponse>,
-                response: Response<PokemonListResponse>
-            ) {
-                binding.progressBarVisibility = false
-                val pokemonList: List<Pokemon> = response.body()!!.pokemonList
-                val adapter = PokemonAdapter(pokemonList)
-                binding.pokemonAdapter = adapter
-                binding.recyclerViewPokemonList.setHasFixedSize(true)
-                val layoutManager = LinearLayoutManager(this@MainActivity)
-                binding.recyclerViewPokemonList.layoutManager = layoutManager
-            }
+    private fun setupObservers() {
+        viewModel.pokemonList.observe(this) { pokemonList ->
+            binding.pokemonAdapter?.pokemonList = pokemonList
+        }
 
-            override fun onFailure(call: Call<PokemonListResponse>, t: Throwable) {
-                binding.progressBarVisibility = false
-                Log.e(Constants.DEBUG_POKEMON, t.message ?: "Unknown error")
-                Toast.makeText(
-                    this@MainActivity,
-                    "Failed to fetch Pokemon list. Please check your internet connection.",
-                    Toast.LENGTH_SHORT
-                ).show()
+        viewModel.isLoading.observe(this) { isLoading ->
+            binding.progressBarVisibility = isLoading
+        }
+
+        viewModel.errorMessage.observe(this) { errorMessage ->
+            if (errorMessage != null) {
+                Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
             }
-        })
+        }
+    }
+
+    private fun setupRecyclerView() {
+        val adapter = PokemonAdapter(emptyList())
+        binding.pokemonAdapter = adapter
+        binding.recyclerViewPokemonList.setHasFixedSize(true)
+        val layoutManager = LinearLayoutManager(this@MainActivity)
+        binding.recyclerViewPokemonList.layoutManager = layoutManager
     }
 }
